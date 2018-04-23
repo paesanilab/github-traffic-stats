65906202206f61a97538f831f1f18ecd67a73e79
Andrea Zonca <code@andreazonca.com>
2018-04-23 13:26:31 -0700

    automatic push

--- collect_stats.sh
+++ collect_stats.sh
@@ -2,3 +2,6 @@ REPO=$1
 cd ~/github-traffic-stats/$REPO
 source ~/github-traffic-stats/tok.sh
 ~/anaconda3/bin/gts "zonca:$TOK" $REPO 'save_csv' -o paesanilab
+git add .
+git commit -m "updated with cron"
+git push
