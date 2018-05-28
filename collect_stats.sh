d7b08bf1b93f4f003993ee4d27e6153daabd2dff
Andrea Zonca <code@andreazonca.com>
2018-05-28 12:14:08 -0700

    Make sure one argument is there

--- collect_stats.sh
+++ collect_stats.sh
@@ -1,3 +1,10 @@
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
 REPO=$1
 cd ~/github-traffic-stats/$REPO
 source ~/github-traffic-stats/tok.sh
